36be5df8b879ab6271ff191a3ce0a217254df64b
therightman <therightmandev@gmail.com>
2018-03-24 01:37:29 +0000

    added index.html and favicon

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function(req, res, next) {
-	try{
-		var html = '<h1>Hey</h1>'
-		res.send(html)
-	} catch(e) {
-		next(e)
-	}
+	res.sendFile('index.html')
 })
 
 app.listen(3000, function() {
